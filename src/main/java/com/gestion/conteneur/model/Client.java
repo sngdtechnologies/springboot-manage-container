f73a1c111da2975d1ac240e65722e32b01212115
Gilles Descartes <sngdtechnologies@gmail.com>
2024-11-19 11:22:37 +0100

    save change

--- src/main/java/com/gestion/conteneur/model/Client.java
+++ src/main/java/com/gestion/conteneur/model/Client.java
@@ -1,11 +1,7 @@
 package com.gestion.conteneur.model;
 
-import com.gestion.conteneur.model.Conteneur;
 import jakarta.persistence.*;
 import jakarta.persistence.Id;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "client")
@@ -16,20 +12,10 @@ public class Client {
     private Long id;
     private String nom;
 
-    @OneToMany(fetch = FetchType.LAZY)
-    private Set<Conteneur> conteneurs = new HashSet<>();
-
     // Getters et setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     
     public String getNom() { return nom; }
     public void setNom(String nom) { this.nom = nom; }
-
-    public Set<Conteneur> getConteneurs() {
-        return this.conteneurs;
-    }
-    public void setConteneurs(Set<Conteneur> conteneurs) {
-        this.conteneurs = conteneurs;
-    }
 }
